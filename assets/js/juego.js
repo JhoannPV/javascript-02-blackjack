6351119f9622848edb8cd5aa55ba685b73834025
JhoannPV <91164583+JhoannPV@users.noreply.github.com>
2026-01-10 08:57:09 -0500

    first commit

---
+++ assets/js/juego.js
@@ -0,0 +1,66 @@
+/**
+ * 2C = Two of Clubs
+ * 2D = Two of Diamonds
+ * 2H = Two of Hearts
+ * 2S = Two of Spades
+ */
+
+
+//Baraja
+let deck = [];
+const tipos = ['C', 'D', 'H', 'S'];
+const especiales = ['A', 'J', 'Q', 'K'];
+
+
+// Función crea un nueva baraja
+const crearDeck = () => {
+    for (let i = 2; i <= 10; i++) {
+        for (let tipo of tipos) {
+            deck.push(i + tipo);
+        }
+    }
+
+    for (let tipo of tipos) {
+        for (let esp of especiales) {
+            deck.push(esp + tipo);
+        }
+    }
+
+    deck = _.shuffle(deck);
+
+    console.log(deck);
+
+    return deck;
+}
+
+crearDeck();
+
+// Esta función me permite tomar una carta
+
+const pedirCarta = () => {
+    if (deck.length === 0) {
+        throw 'No hay cartas en el deck';
+    }
+    const carta = deck.pop();
+
+    console.log(deck);
+    console.log(carta);
+
+    return carta;
+}
+
+// pedirCarta();
+
+const valorCarta = (carta) => {
+    // Se puede usar slice en vez de substring
+    const valor = carta.substring(0, carta.length - 1);
+    return (isNaN(valor)) ?
+        (valor === 'A') ? 1
+            : (valor === 'J') ? 11
+                : (valor === 'Q') ? 12
+                    : 13
+        : valor * 1;
+}
+
+const valor = valorCarta(pedirCarta());
+console.log({ valor });
